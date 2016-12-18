ae0b406189177380480154a9c2beb06d7287bc93
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2016-12-18 21:38:55 +0200

    move js error handler (shows js errors as alerts) into separate package
    - integrate https://github.com/oprudkyi/js-error-alert
    - add JS_ERROR_ALERTER_ENABLED .env variable
    - remove old js

--- gulpfile.js
+++ gulpfile.js
@@ -23,10 +23,11 @@ var paths = {
 , 'fontawesome': './vendor/bower_components/font-awesome/'
 , 'datatables': './vendor/bower_components/datatables/media/'
 , 'adminlte': './vendor/bower_components/AdminLTE/dist/'
+, 'js_error_alert': './vendor/bower_components/js-error-alert/'
 , 'bootstrap_notify': './vendor/bower_components/remarkable-bootstrap-notify/dist/'
 , 'bower_components': './vendor/bower_components/'
 , 'starterkit': './resources/assets/sass/starterkit/'
-}
+};
 
 elixir(function(mix) {
     mix.sass('app.scss', 'public/css/'
@@ -42,9 +43,9 @@ elixir(function(mix) {
         .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap')
         .copy(paths.fontawesome + 'fonts/**', 'public/fonts/fontawesome')
         .scripts([
-			'./resources/javascripts/error/_error_handler.js',
+			paths.js_error_alert + 'window_error_handler.js',
             paths.jquery + "dist/jquery.js",
-			'./resources/javascripts/error/jquery_error_handler.js',
+			paths.js_error_alert + 'jquery_error_handler.js',
             paths.bower_components + "moment/min/moment.min.js",
             paths.datatables + "js/jquery.dataTables.js",
             paths.bootstrap + "javascripts/bootstrap.js",
--- resources/views/layouts/partials/auth/jsvariables.blade.php
+++ resources/views/layouts/partials/auth/jsvariables.blade.php
@@ -1,33 +1,6 @@
 {{-- this is js stuff loaded a top --}}
 <script>
-/*
- * Main error handler, helps to popup errors earlier
- */
-
-var showUncaughtException = function(message) {
-	"use strict";
-
-	if(typeof message == "undefined") {
-		return false;
-	}
-	alert(message);
-}
-
-window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-	"use strict";
-
-	if(typeof errorMsg == "undefined") {
-		return false;
-	}
-
-	showUncaughtException("JS Error : " + errorMsg + "\n" +
-		"line number : " + lineNumber + "\n" +
-		"column : " + column + "\n" +
-		"object : " + errorObj
-		);
-        
-    // Tell browser to run its own error handler as well   
-    return false;
-}
+//enable js-error-alert
+var JSEH_enabled = @if(env('JS_ERROR_ALERTER_ENABLED', false)) true @else false @endif ;
 </script>
 
