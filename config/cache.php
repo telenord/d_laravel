0157a9dbc8a376f24344a48de0ffbec202cce4bc
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2017-04-12 10:07:47 +0300

    fix error - Memcached::addServer() expects parameter 2 to be integer, string given

--- config/cache.php
+++ config/cache.php
@@ -62,7 +62,7 @@ return [
             'servers' => [
                 [
                     'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
+                    'port' => intval(env('MEMCACHED_PORT', 11211)),
                     'weight' => 100,
                 ],
             ],
