b08c76af3b13624128ac8129a7963ca8534756db
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2017-04-13 07:19:08 +0300

    on deployment don't resolve symlinks

--- deploy_production.sh
+++ deploy_production.sh
@@ -8,13 +8,16 @@ set -e
 
 #get paths (if run not from correct directory)
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
 
-export ROOT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#use code below to resolve symlinks
+#while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+#  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#  SOURCE="$(readlink "$SOURCE")"
+#  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+#done
+#export ROOT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+export ROOT_DIR="$( cd -L "$( dirname "$SOURCE" )" && pwd )"
 
 cd "$ROOT_DIR"
     
