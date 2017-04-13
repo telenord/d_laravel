63d55b0ea89d8e13b9705efd56f06c8b6807e4a4
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2017-04-13 04:53:45 +0300

    deployment to prod via GitLab CI
    - ssh/git way
    - deploy_production.sh to run on server to update
    - very simple approach, in case of errors
    requires manual intervention (might leave server in maintenance mode)

---
+++ deploy_production.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+#Simple git based deployment
+# - it should be run on server
+# - the server source should be configured to have access to git (as well correct branch) and tools
+# - it's very optimistic for errors and will break/stop if something wrong (and leave site in maintenance mode), then you have to fix manually
+
+#get paths (if run not from correct directory)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+export ROOT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "$ROOT_DIR"
+    
+echo "Deploy production"
+
+#check if git works
+git ls-remote &> /dev/null
+
+php artisan down
+git pull
+make install-dependencies-production
+php artisan migrate --force --no-interaction --verbose
+make production-optimize
+php artisan up
+
+echo "Production has been updated successfuly"
